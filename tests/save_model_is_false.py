e72f3b7c805efbfefc607cb0c5e20eb6b821550c
Kayenga Campos <31307359+Kissabi@users.noreply.github.com>
2023-03-04 20:24:39 +0100

    Added file save_model_is_false

---
+++ tests/save_model_is_false.py
@@ -0,0 +1,24 @@
+
+import unittest
+from fast_ner import Fast_NER
+
+class TestFastNER(unittest.TestCase):
+
+    def test_save_model_false(self):
+        # Define a phrase and set save_model to False
+        phrase = "Steve played a pivotal role in the development of Apple."
+        ner = Fast_NER(phrase=phrase, save_model=False)
+
+        # Load entities file in json format
+        ent = ner.read_json("ent_list.json")
+
+        # Process the text data to associate entities labels
+        model = ner.process_text(ent)
+
+        # Train the model
+        ner.train(model)
+
+        # Verify that the file does not exist on disk
+        self.assertFalse(os.path.exists('./train.spacy'))
+
+unittest.main(argv=[''], verbosity=3, exit = False)
